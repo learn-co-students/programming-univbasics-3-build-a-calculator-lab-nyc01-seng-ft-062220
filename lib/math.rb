f008a4fed9dfa4ee1a1b4eae6f8f006492cf50ca
fearless-command-2358 <iuriseara@gmail.com>
2020-05-26 22:44:09 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  x = num1 + num2
+  puts x
+  return x
 end
 
 def subtraction(num1, num2)
-
+  x = num1 - num2
+  puts x
+  return x
 end
 
 def division(num1, num2)
-
+  x = num1 / num2
+  puts x
+  return x
 end
 
 def multiplication(num1, num2)
-
+  x = num1 * num2
+  puts x
+  return x
 end
 
 def modulo(num1, num2)
-
+  x = num1 % num2
+  puts x
+  return x
 end
 
 def square_root(num)
-
+  x = Math.sqrt(num)
+  puts x
+  return x
 end
